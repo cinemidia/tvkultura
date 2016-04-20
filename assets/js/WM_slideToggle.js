82cd4f7fa12d254a7ce4992885bc767bc04e39d0
wilton <wilto94@gmail.com>
2016-04-20 10:52:07 -0100

    menu_fixo

--- assets/js/WM_slideToggle.js
+++ assets/js/WM_slideToggle.js
@@ -1,24 +1,30 @@
 jQuery(document).ready(function() {
-	   $('nav').hide();
+	   
   	$("body").on('click', '#btn_menu', function(event) {
       event.preventDefault();
       $("nav").slideToggle();
      
-    });
-      
+    });      
    
 
     $(window).resize(function() {
         if($(window).width() > 783){
-           $('nav').show();            
-
+           $('nav').show();           
         }
         else{
            $('nav').hide();
         }
-                 
-                         
-    });
+     });
+
+       
+     var nav = $('.Navbar');    
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 136) {
+            nav.addClass("f-nav");
+         } else {
+            nav.removeClass("f-nav");
+        }
+     });
 
   
 });
